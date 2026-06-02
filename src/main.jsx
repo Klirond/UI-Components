4bee61946eae91b1cb9d0892fd444319d7d2248f
Shaedow2000 <shaedow2000@proton.me>
2026-06-02 12:53:59 +0100

    Add tailwindcss.

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import Root from "./layout/Root.jsx";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Root />
+    <section></section>
   </StrictMode>,
 );
