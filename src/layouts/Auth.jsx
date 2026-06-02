1d2a732c4070e4d983bf491f80f3e21175efc2c3
Shaedow2000 <shaedow2000@proton.me>
2026-06-02 12:57:13 +0100

    Create auth layout and styles.

---
+++ src/layouts/Auth.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import Footer from "../components/Footer";
+import LoadingScreen from "../components/LoadingScreen";
+import "./layouts.css";
+
+import { Outlet } from "react-router";
+import { useNavigation } from "react-router";
+import { useState } from "react";
+
+function Auth() {
+  const navigation = useNavigation();
+  const [isNavigating, setIsNavigating] = useState(
+    navigation.state === "loading" || navigation.state === "submitting",
+  );
+
+  useEffect(() => {
+    setIsNavigating(
+      navigation.state === "loading" || navigation.state === "submitting",
+    );
+  }, [navigation.state]);
+
+  return (
+    <>
+      {isNavigating && <LoadingScreen />}
+      <main id="auth-layout">
+        {window.innerWidth >= 768 ? (
+          <div className="desktop-section">
+            <Outlet />
+          </div>
+        ) : (
+          <Outlet />
+        )}
+        <Footer />
+      </main>
+    </>
+  );
+}
+
+export default Auth;
